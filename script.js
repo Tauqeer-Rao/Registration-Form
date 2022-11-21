2713a1e2de23ffe1fb5f5fec287cac1e1e8899a6
Mian Wasif <you@example.com>
2022-11-21 11:33:46 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function myFunction() {
+    var a = document.getElementById("d1").value;
+    var b = document.getElementById("d2").value;
+    var c = document.getElementById("d3").value;
+    var d = document.getElementById("d4").value;
+    document.getElementById("r1").innerHTML = a;
+    document.getElementById("r2").innerHTML = b;
+    document.getElementById("result").innerHTML = c;
+    document.getElementById("result").innerHTML = d;
+}
